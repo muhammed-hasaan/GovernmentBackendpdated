92df876137ce721fee62369ce8e90176f4d363d6
Hasaan Developer <muhammedhasaan09@gmail.com>
2024-02-13 14:47:44 +0500

    Update imageDetails.js

--- imageDetails.js
+++ imageDetails.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
-const ImageDetailsScehma = new mongoose.Schema(
+
+const ImageDetailsSchema = new mongoose.Schema(
   {
-   image:String,
-   name:String,
-   select:String,
-   password:String,
-   fileid:String,
-   date:String,
-   code:String,
-   location:String,
-   QrGet:String,
-   Qrcode:String
+    image:String,
+    name: String,
+    select: String,
+    password: String,
+    fileid: String,
+    date: String,
+    code: String,
+    location: String,
+    QrGet: String,
+    Qrcode: String,
+    image2:String,
+    image3:String
   },
   {
     collection: "ImageDetails",
   }
 );
 
-mongoose.model("ImageDetails", ImageDetailsScehma);
+mongoose.model("ImageDetails", ImageDetailsSchema);
